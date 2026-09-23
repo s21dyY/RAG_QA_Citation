3079714ae298b957f2c9f93cbac77b34cb2b0133
Sandy Yang <sandy.yang992@gmail.com>
2026-09-22 23:17:42 -0400

    add api.py

--- src/api.py
+++ src/api.py
@@ -1,3 +1,19 @@
 
-import fastapi 
-import uvicorn
+from fastapi import FastAPI
+from src.retrieve import get_collection, retrieve
+from sentence_transformers import SentenceTransformer
+
+# uvicorn src.api:app --reload
+app = FastAPI()
+model = None
+collection = None
+
+@app.on_event("startup")
+def startup():
+    global model,collection
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    collection = get_collection()
+
+@app.get("/search")
+def search(q:str, k:int=5):
+    return retrieve(q, model, collection,k)
